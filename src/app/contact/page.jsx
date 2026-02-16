f5bb8ad1a7c52809579316942038e48e9119565d
Saiful Saif <saif@Saifuls-Mac-mini.local>
2026-02-16 14:49:16 +0600

    format code in contact page component

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ContactHero from '../components/contact/contactHero'
-import LocationMap from '../components/contact/LocationMap'
+import React from "react";
+
+import ContactHero from "../components/contact/contactHero";
+import LocationMap from "../components/contact/LocationMap";
 
 function page() {
   return (
     <div>
-      <ContactHero/>
-      <LocationMap/>
+      <ContactHero />
+      <LocationMap />
     </div>
-  )
+  );
 }
 
 export default page
