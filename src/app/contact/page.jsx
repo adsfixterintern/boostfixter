162a1d504f707fca79704f2f5b6593190df7b63f
Saiful Saif <mdsaiful.adsfixter@gmail.com>
2026-02-16 16:15:00 +0600

    replace useRouter with usePathname and enhance active link styling feat(contact): add NeedConsultation component to contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import ContactHero from "../components/contact/contactHero";
 import LocationMap from "../components/contact/LocationMap";
+import NeedConsultation from "../services/service/NeedConsultation";
 
 function page() {
   return (
     <div>
       <ContactHero />
       <LocationMap />
+      <NeedConsultation />
     </div>
   );
 }
