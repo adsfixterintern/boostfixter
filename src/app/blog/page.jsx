181c461b16eb899732deb75150af1cfa2be9e315
Hasan Rafi Ahmed <rafi@Hasans-Mac-mini.local>
2026-02-16 14:41:21 +0600

    in blog page add need consultrtation

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AllBlog from '../components/shared/blogs/AllBlog'
 import InsightStory from '../contact/InsightStory'
+import NeedConsultation from '../services/service/NeedConsultation'
 
 export default function page() {
   return (
     <div>
         <AllBlog></AllBlog>
         <InsightStory></InsightStory>
+        <NeedConsultation></NeedConsultation>
     </div>
   )
 }
